f9484aa885d756be809b1d9c300f1637ac96f05e
onceaba <gulsah2009sahin@gmail.com>
2026-07-14 16:04:43 +0300

    GitHub Pages yolu düzeltildi

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite' // Tailwind kullanıyorsun, bunu da ekle
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: '/pspsps/', // <-- BU SATIR ÇOK ÖNEMLİ
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
 })
